a7148ed067901d34553e404b19c5e801e76e47d9
dvory weiss <d0583237262@gmail.com>
2025-02-24 21:17:42 +0200

    update cartSlice
    Adding the option to change the quantity of products in the shopping cart

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -18,9 +18,16 @@ const cartSlice = createSlice({
             } else {
                 state.arr.push({ ...action.payload ,qty:qty});
             }
+        },
+        updateQuantity: (state, action) => {
+            let { _id, qty } = action.payload; 
+            let index = state.arr.findIndex(item => item._id == _id);
+            if (index > -1) {
+            state.arr[index].qty = qty;
+            } 
         }
     }
 });
 
-export const  {addToCart} = cartSlice.actions;
+export const  {addToCart,updateQuantity} = cartSlice.actions;
 export default cartSlice.reducer;
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,27 +1,31 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { updateQuantity } from "../features/cartSlice"; // עדכני בהתאם למיקום
 
 const Cart = () => {
     const cart = useSelector(state => state.cart.arr);
+    const dispatch = useDispatch();
 
-    console.log("תוכן העגלה:", cart); // זה יראה את הנתונים המעודכנים
+    const changeQty = (id, newQty) => {
+        dispatch(updateQuantity({ _id: id, qty: Number(newQty) }));
+    };
 
     return (
         <div className="cart-div">
             <h3>עגלת קניות</h3>
             <ul>
-            {cart.map(item => (
-                <li key={item._id}>
-                    <p>{item.productName} </p>
-                    <p> כמות: {item.qty}</p>
-                    {/* <input 
-                    className="cnt-products"
-                    type="number" 
-                    value={item.qty} 
-                    min="1"
-                    onChange={(e) => changeQty=()=>{(item.qty=e.target.value)}} 
-                    /> */}
-                </li>
-            ))}
+                {cart.map(item => (
+                    <li key={item._id}>
+                        <p>{item.productName} </p>
+                        <p> כמות: {item.qty}</p>
+                        <input 
+                            className="cnt-products"
+                            type="number" 
+                            value={item.qty} 
+                            min="1"
+                            onChange={(e) => changeQty(item._id, e.target.value)}
+                        />
+                    </li>
+                ))}
             </ul>
         </div>
     );
