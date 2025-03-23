819cb0e800a5f50e31f0098d313b0e45f56f9dde
Brachy <b0583286293@gmail.com>
2025-03-23 16:05:36 +0200

    change orderService

--- src/api/orderService.js
+++ src/api/orderService.js
@@ -1,9 +1,16 @@
 import axios from "axios"
+import { useSelector } from "react-redux";
 
 
 let baseUrl="https://project-in-node.onrender.com/api/orders";
 
 export const httpAddOrder = (order,token) => {
+    order={
+        destDate : new Date(new Date().setDate(new Date().getDate() + 7)),
+        address: order.shippingStreet + " " + order.shippingHouse + " " + order.shippingCity,
+        cust_id: JSON.parse(localStorage.getItem("user"))._id,
+        products: useSelector(state => state.cart.arr),
+    }
     return axios.post(baseUrl,order,{
         headers:{
             "Authorization" : `Bearer ${token}`
