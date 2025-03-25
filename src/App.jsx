2642f67dc839e335ae0579273556ce9dd283d6fc
dvory weiss <d0583237262@gmail.com>
2025-03-25 10:39:05 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'
-import ProductList from './pages/ProductList'
+import './App.css';
+import ProductList from './pages/ProductList';
 import Cart from './pages/cart';
-import { Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -12,9 +12,7 @@ import Add from './pages/Add';
 import UpdateProduct from './pages/Update';
 import SmallCart from './pages/SmallCart';
 import Order from './pages/Order';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import Logout from "./pages/Logout"
+import Logout from "./pages/Logout";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
@@ -30,12 +28,6 @@ const theme = createTheme({
       },
   },
 });
-=======
-import Logout from './pages/Logout';
->>>>>>> Stashed changes
-=======
-import Logout from './pages/Logout';
->>>>>>> Stashed changes
 
 function App() {
   let listCategoties = {
@@ -46,7 +38,6 @@ function App() {
   };
   
   return (
-<<<<<<< Updated upstream
     <> 
       <ThemeProvider theme={theme}>
           <NavBar categoties={listCategoties} theme={theme}/>
@@ -71,36 +62,9 @@ function App() {
               <Route path='/add' element={<Add/>}/> 
               <Route path='/update/:id' element={<UpdateProduct/>}/>
               <Route path='/order' element={<Order/>}/>
-              <Route path='logout' element={<Logout/>}/>               
+              <Route path='/logout' element={<Logout/>}/>    
           </Routes>
       </ThemeProvider>
-=======
-    <>
-      <NavBar categoties={listCategoties}/>
-      <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/products" element={<ProductList/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/table" element={<ProductList category='table'/>}/>
-          <Route path="/livingRoom" element={<ProductList category='livingRoom'/>}/>
-          <Route path="/accessories" element={<ProductList category='accessories'/>}/>
-          <Route path="/packages" element={<ProductList category='packages'/>}/>
-          <Route path="/tablecloths" element={<ProductList category='tablecloths'/>}/>
-          <Route path="/placement" element={<ProductList category='placement'/>}/>
-          <Route path="/fragrance" element={<ProductList category='fragrance'/>}/>
-          <Route path="/flower" element={<ProductList category='flower'/>}/>
-          <Route path="/home" element={<ProductList category='home'/>}/>
-          <Route path="/candlesticks" element={<ProductList category='candlesticks'/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/Signup" element={<Signup/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path='/add' element={<Add/>}/> 
-          <Route path='/update/:id' element={<UpdateProduct/>}/>
-          <Route path='/smallCart' element={<SmallCart/>}/>
-          <Route path='/order' element={<Order/>}/>               
-           <Route path='/logout' element={<Logout/>}/>    
-      </Routes>
->>>>>>> Stashed changes
     </>  
   );
 }
--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,55 +1,23 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { userOut } from "../features/userSlice";
 import { clearCart } from "../features/cartSlice"; 
 import { useNavigate } from "react-router-dom";
-=======
-=======
->>>>>>> Stashed changes
-import { userOut } from "../features/userSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom"
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 const Logout = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     useEffect(() => {
         dispatch(userOut());
         dispatch(clearCart());
-        navigate("/Products");
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        localStorage.removeItem("cart");
+        navigate("/products");
     }, [dispatch, navigate]);
 
     return null;
 };
 
 export default Logout;
-=======
-=======
->>>>>>> Stashed changes
-    dispatch(userOut);
-    localStorage.setItem("user",null)
-    localStorage.setItem("token",null)
-    localStorage.setItem("cart",[])
-
-    navigate("/Products")
-
-
-    return ( <></> );
-}
- 
-<<<<<<< Updated upstream
-export default Logout;
->>>>>>> Stashed changes
-=======
-export default Logout;
->>>>>>> Stashed changes
